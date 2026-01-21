4129a5908a0d2d65fc4af8da6fc78308fed49ce9
mkagri-26 <almasasahalkawmia@gmail.com>
2026-01-21 23:13:51 +0200

    Delete lang.js
    Signed-off-by: mkagri-26 <almasasahalkawmia@gmail.com>

--- lang.js
+++
@@ -1,1 +0,0 @@
-const switcher=document.getElementById("languageSwitcher");if(switcher){switcher.addEventListener("change",async()=>{const lang=switcher.value;const response=await fetch(`lang/${lang}.json`);const data=await response.json();document.querySelectorAll("[data-lang]").forEach(el=>{el.textContent=data[el.getAttribute("data-lang")];});});}
